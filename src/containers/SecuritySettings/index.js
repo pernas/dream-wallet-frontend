868b7cfa119503410b0df5b6efe04eeb97272832
Justin Tormey <justin@blockchain.com>
2017-06-01 18:58:54 -0400

    ui for /settings/security route

--- src/containers/SecuritySettings/index.js
+++ src/containers/SecuritySettings/index.js
@@ -2,9 +2,16 @@ import React from 'react'
 import SecuritySettings from 'components/SecuritySettings'
 
 class SecuritySettingsContainer extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { advanced: false }
+  }
+
   render () {
+    let { advanced } = this.state
+    let toggleAdvanced = () => this.setState({ advanced: !advanced })
     return (
-      <SecuritySettings />
+      <SecuritySettings advanced={advanced} toggleAdvanced={toggleAdvanced} />
     )
   }
 }
