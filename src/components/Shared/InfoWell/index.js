005300a37f6acdba59c4ee6150e41ad31e59c373
Justin Tormey <justin@blockchain.com>
2017-06-01 18:03:44 -0400

    ui for /settings/info route

---
+++ src/components/Shared/InfoWell/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.scss'
+
+const InfoWell = ({ children }) => (
+  <div className='info-well type-h5 em-500'>
+    {children}
+  </div>
+)
+
+export default InfoWell
