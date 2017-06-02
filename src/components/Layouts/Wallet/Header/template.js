521f850c71ab553dffd05d583f910b1834e76c0f
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2017-06-02 01:10:47 +0100

    interim commit

--- src/components/Layouts/Wallet/Header/template.js
+++ src/components/Layouts/Wallet/Header/template.js
@@ -1,51 +1,24 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
 
+import ExploreMenu from './ExploreMenu'
 import logo from 'img/blockchain-vector.svg'
-import './style.scss'
+import style from './style.scss'
 
-const Header = (props) => {
+const Header = () => {
   return (
-    <header className='wallet'>
-      <nav className='navbar navbar-default'>
-        <div className='container-fluid'>
-          <div className='navbar-header'>
-            <button type='button' className='navbar-toggle collapsed'>
-              <span className='sr-only'>Toggle navigation</span>
-              <span className='icon-bar' />
-              <span className='icon-bar' />
-              <span className='icon-bar' />
-            </button>
-            <NavLink to='/' className='navbar-brand'>
-              <img src={logo} />
-            </NavLink>
-          </div>
-          <ul className='nav navbar-nav navbar-right'>
-            <li className={(props.exploreMenuDisplayed ? 'dropdown open' : 'dropdown')}>
-              <a href='#' className='dropdown-toggle' onClick={props.clickExploreMenu}>
-                <span>Explore</span>
-                <i className='ti-angle-down mlm' />
-              </a>
-              <ul className='dropdown-menu'>
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>Charts</a></li>
-                <li><a href='#'>Stats</a></li>
-                <li><a href='#'>Markets</a></li>
-                <li><a href='#'>API</a></li>
-              </ul>
-            </li>
-            <li><a href='#'><span>Sign out</span></a></li>
-          </ul>
-        </div>
-      </nav>
+    <header className={style.header}>
+      <div className={style.left}>
+        <NavLink to='/'>
+          <img src={logo} className={style.logo} />
+        </NavLink>
+      </div>
+      <div className={style.right}>
+        <ExploreMenu />
+        <a className={style.link}>Sign out</a>
+      </div>
     </header>
   )
-}
-
-Header.propTypes = {
-  exploreMenuDisplayed: PropTypes.bool.isRequired,
-  clickExploreMenu: PropTypes.func.isRequired
 }
 
 export default Header
